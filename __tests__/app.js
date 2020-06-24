542e8bd1c762fe07ea1554136308acfadb8ebc1b
Hudson Cassidy <hudoc96@hotmail.com>
2020-06-24 19:59:22 +1000

    added package.json to the tests. fixed install folder being ouside the option passed in parent folder

--- __tests__/app.js
+++ __tests__/app.js
@@ -18,6 +18,7 @@ describe('generator-node-graphql:app', () => {
 
   it('creates files', () => {
     assert.file([
+      'package.json',
       '.env',
       '.gitignore',
       '.eslintrc.json',
@@ -48,19 +49,19 @@ describe('generator-node-graphql:app commandLine', () => {
   });
 
   it('creates files in the given directory', () => {
-    const dir = 'testDir/';
     assert.file([
-      dir + '.env',
-      dir + '.gitignore',
-      dir + '.eslintrc.json',
-      dir + 'tsconfig.json',
-      dir + 'README.md',
-      dir + 'src/server.ts',
-      dir + 'src/models/userModel.ts',
-      dir + 'src/graphql/schemas/user.graphql',
-      dir + 'src/graphql/resolvers/userResolver.ts',
-      dir + 'tests/dbHandler.ts',
-      dir + 'tests/resolvers/userResolver.test.ts',
+      'package.json',
+      '.env',
+      '.gitignore',
+      '.eslintrc.json',
+      'tsconfig.json',
+      'README.md',
+      'src/server.ts',
+      'src/models/userModel.ts',
+      'src/graphql/schemas/user.graphql',
+      'src/graphql/resolvers/userResolver.ts',
+      'tests/dbHandler.ts',
+      'tests/resolvers/userResolver.test.ts',
     ]);
   });
 });
